5d572360795b57d1bc84a82cf4b1d0052717f6ae
Yuchen <u7663065@anu.edu.au>
2024-03-10 16:20:16 +1100

    State_design_pattern

---
+++ PaymentPendingState.java
@@ -0,0 +1,7 @@
+public class PaymentPendingState implements VendingMachineState{
+
+    @Override
+    public void handleRequest() {
+        System.out.println("Payment pending state: Dispensing product.");
+    }
+}
---
+++ ReadyState.java
@@ -0,0 +1,7 @@
+public class ReadyState implements VendingMachineState{
+    @Override
+    public void handleRequest() {
+        System.out.println("Ready state: Please select a product.");
+    }
+}
+
